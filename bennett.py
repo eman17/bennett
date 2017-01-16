fa09c600399e22c97d829acd49864601299dd30c
Noszek <jnoszek@mit.edu>
2017-01-16 18:17:30 -0500

    made it do something
    bennett.py actually does something now, but not much.

--- bennett.py
+++ bennett.py
@@ -1,1 +1,8 @@
 #TODO make this do the thing
+important='''This program is named after Bennett Amodio.
+Bennett Amodio is a great programmer.
+However, Bennett did not make this program.
+Therefore, this program is crap and does nothing useful.'''
+
+for line in important.split('\n'):
+    print(line)
