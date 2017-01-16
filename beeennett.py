dceb592b29bb87cefa857ccab3916907e9dfb383
Noszek <jnoszek@mit.edu>
2017-01-16 16:20:47 -0500

    add beeennett
    The new, improved version of beenett

---
+++ beeennett.py
@@ -0,0 +1,158 @@
+from time import sleep
+from random import randint
+
+important = '''According to all known laws
+of aviation,
+  
+there is no way a bee
+should be able to fly.
+  
+Its wings are too small to get
+its fat little body off the ground.
+  
+The bee, of course, flies anyway
+  
+because bees don't care
+what humans think is impossible.
+  
+Yellow, black. Yellow, black.
+Yellow, black. Yellow, black.
+
+NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+Why, Cory?
+
+Why, Cory?
+
+Why?
+
+
+
+'''
+for line in important.split('\n') :	
+	print(line)
+	sleep(randint(0,500)/1000)
